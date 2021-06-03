a6198be7639400051239f0d463dc59ed449df374
Jhonatan Alexander <jhonatan.hernandez@eyss.io>
2021-06-03 10:46:08 -0400

    added test BSC network

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,5 +22,9 @@ module.exports = {
       url: process.env.RINKEBY_ALCHEMY_KEY,
       accounts: { mnemonic: process.env.MY_MNEMONIC },
     },
+    testBSC: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      accounts: { mnemonic: process.env.MY_MNEMONIC },
+    },
   },
 }
